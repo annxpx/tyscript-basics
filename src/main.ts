4d207541c43e1f1b117f76394444e152aea362ad
Andre <apalvaradol@outlook.com>
2022-10-10 11:12:48 -0600

    Clase 008: Recorrer Arreglos

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 //import Tipos from "./tipos";
-import {imprimirNombres, contarPosiciones} from './arreglos';
-imprimirNombres();
-contarPosiciones();
+//import {imprimirNombres, contarPosiciones} from './arreglos';
+//mprimirNombres();
+//contarPosiciones();
+import {listarArray, listarString, listarLoop} from "./arreglos2";
+//listarArray();
+//listarString();
+listarLoop();
+
+
 //const tipos = new Tipos(2,2);
 //console.log('la suma de 4 y 4 es: ' + tipos.sumar(4,4));
